86730408842ec290d1a749abea2338a8d137c678
Ali Soomar <alisoomar@gmail.com>
2021-01-23 21:04:27 -0600

    login done

--- dist/auth/router.js
+++ dist/auth/router.js
@@ -13,6 +13,8 @@ var _passport = _interopRequireDefault(require("passport"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+const crypto = require('crypto');
+
 const User = _mongoose.default.model('user');
 
 const router = (0, _express.Router)();
@@ -67,5 +69,173 @@ router.post('/initialize', async (req, res, next) => {
     message: 'error saving user'
   }));
 });
+router.post('/register', async (req, res, next) => {
+  const {
+    body: {
+      user
+    }
+  } = req;
+
+  if (!user.email) {
+    return res.status(422).json({
+      errors: {
+        email: 'is required'
+      }
+    });
+  }
+
+  if (!user.password) {
+    return res.status(422).json({
+      errors: {
+        password: 'is required'
+      }
+    });
+  }
+
+  if (!user.firstName) {
+    return res.status(422).json({
+      errors: {
+        firstName: 'is required'
+      }
+    });
+  }
+
+  if (!user.lastName) {
+    return res.status(422).json({
+      errors: {
+        lastName: 'is required'
+      }
+    });
+  }
+
+  const query = await User.find({
+    email: user.email
+  });
+
+  if (query.length == 0) {
+    return res.status(422).json({
+      success: false,
+      errors: {
+        email: 'not registered'
+      }
+    });
+  }
+
+  if (!query[0].isActive) {
+    return res.status(422).json({
+      success: false,
+      errors: {
+        user: 'not active'
+      }
+    });
+  }
+
+  if (query[0].isRegistered) {
+    return res.status(422).json({
+      success: false,
+      errors: {
+        user: 'already registered'
+      }
+    });
+  }
+
+  const {
+    salt,
+    hash
+  } = setPassword(user.password);
+  return User.updateOne({
+    _id: query[0]._id
+  }, {
+    firstName: user.firstName,
+    lastName: user.lastName,
+    isRegistered: true,
+    salt: salt,
+    hash: hash
+  }).then(() => res.send({
+    success: true,
+    message: 'registration done'
+  })).catch(e => res.status(422).json({
+    success: false,
+    errors: e,
+    message: 'error registering user'
+  }));
+});
+router.post('/login', async (req, res, next) => {
+  const {
+    body: {
+      user
+    }
+  } = req;
+
+  if (!user.email) {
+    return res.status(422).json({
+      errors: {
+        email: 'is required'
+      }
+    });
+  }
+
+  if (!user.password) {
+    return res.status(422).json({
+      errors: {
+        password: 'is required'
+      }
+    });
+  }
+
+  const query = await User.find({
+    email: user.email
+  });
+
+  if (query.length == 0) {
+    return res.status(422).json({
+      success: false,
+      errors: {
+        email: 'incorrect email'
+      }
+    });
+  }
+
+  if (!query[0].isActive) {
+    return res.status(422).json({
+      success: false,
+      errors: {
+        user: 'not active'
+      }
+    });
+  }
+
+  if (!query[0].isRegistered) {
+    return res.status(422).json({
+      success: false,
+      errors: {
+        user: 'not registered'
+      }
+    });
+  }
+
+  if (!query[0].validatePassword(user.password)) {
+    return res.status(422).json({
+      success: false,
+      errors: {
+        password: 'incorrect password'
+      }
+    });
+  }
+
+  res.send({
+    success: true,
+    user: query[0].toAuthJSON()
+  });
+});
+
+function setPassword(password) {
+  let saltAndHash = {};
+  saltAndHash.salt = crypto.randomBytes(16).toString('hex');
+  saltAndHash.hash = crypto.pbkdf2Sync(password, saltAndHash.salt, 10000, 512, 'sha512').toString('hex');
+  return saltAndHash;
+}
+
+;
 var _default = router;
 exports.default = _default;
--- src/auth/router.js
+++ src/auth/router.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import mongoose from 'mongoose'
 import passport from 'passport'
+const crypto = require('crypto');
 
 const User = mongoose.model('user');
 
@@ -121,13 +122,98 @@ router.post('/register', (req, res, next) => {
   //   return res.json({ auth: 'failure' });
   // })(req, res, next);
 
-  res.send({ express: 'Test call to backend' })
-//   finalUser.setPassword(user.password);
-  return finalUser.save()
-    .then(() => res.json({ user: finalUser.toAuthJSON() }))
-    .then(() => res.send({ success: true, message: 'initialize done' }))
-    .catch((e) => res.status(422).json({ success: false, errors: e, message: 'error saving user' }))
+  const query = await User.find({email: user.email});
+  if (query.length == 0){
+    return res.status(422).json({
+      success: false,
+      errors: { email: 'not registered',},
+    });
+  }
+
+  if (!query[0].isActive) {
+    return res.status(422).json({
+      success: false,
+      errors: { user: 'not active',},
+    });
+  }
+
+  if (query[0].isRegistered) {
+    return res.status(422).json({
+      success: false,
+      errors: { user: 'already registered',},
+    });
+  }
+
+  const {salt, hash} = setPassword(user.password)
+
+  return User.updateOne({_id: query[0]._id}, {
+    firstName: user.firstName,
+    lastName: user.lastName,
+    isRegistered: true,
+    salt: salt,
+    hash: hash
+  })
+  .then(() => res.send({ success: true, message: 'registration done' }))
+  .catch((e) => res.status(422).json({ success: false, errors: e, message: 'error registering user' }))
 });
+
+router.post('/login', async (req, res, next) => {
+  const { body: { user } } = req;
+
+  if(!user.email) {
+    return res.status(422).json({
+      errors: {
+        email: 'is required',
+      },
+    });
+  }
+
+  if(!user.password) {
+    return res.status(422).json({
+      errors: {
+        password: 'is required',
+      },
+    });
+  }
+
+  const query = await User.find({email: user.email});
+  if (query.length == 0){
+    return res.status(422).json({
+      success: false,
+      errors: { email: 'incorrect email',},
+    });
+  }
+
+  if (!query[0].isActive) {
+    return res.status(422).json({
+      success: false,
+      errors: { user: 'not active',},
+    });
+  }
+
+  if (!query[0].isRegistered) {
+    return res.status(422).json({
+      success: false,
+      errors: { user: 'not registered',},
+    });
+  }
+
+  if (!query[0].validatePassword(user.password)) {
+    return res.status(422).json({
+      success: false,
+      errors: { password: 'incorrect password',},
+    });
+  }
+
+  res.send({ success: true, user: query[0].toAuthJSON() });
+});
+
+function setPassword(password) {
+  let saltAndHash = {}
+  saltAndHash.salt = crypto.randomBytes(16).toString('hex');
+  saltAndHash.hash = crypto.pbkdf2Sync(password, saltAndHash.salt, 10000, 512, 'sha512').toString('hex');
+  return saltAndHash
+};
 
 
 export default router
