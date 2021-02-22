9fa721a4ce58341a1f3f091742d692ea85330f69
Cem Ergin <birazgecemuzigi@gmail.com>
2021-02-21 22:48:59 -0500

    Users Router.js
    Added _id to /getAllFromRole endpoint

--- src/resources/users/router.js
+++ src/resources/users/router.js
@@ -11,104 +11,135 @@ router
   .put((req, res) => {})
   .delete((req, res) => {})
 
-
 //admin
 
 //create users (initialize) and optionally assign students
 router.post('/initialize', async (req, res, next) => {
-  const { body: { user } } = req;
+  const {
+    body: { user },
+  } = req
 
-  console.log("user", user)
+  console.log('user', user)
 
-  if(!user.email) {
+  if (!user.email) {
     return res.status(422).json({
       success: false,
-      errors: { email: 'is required',},
-    });
+      errors: { email: 'is required' },
+    })
   }
 
-  if(!user.role) {
+  if (!user.role) {
     return res.status(422).json({
       success: false,
       errors: {
         role: 'is required',
       },
-    });
+    })
   }
 
-  const query = await User.find({email: user.email});
-  if (query.length != 0){
+  const query = await User.find({ email: user.email })
+  if (query.length != 0) {
     return res.status(422).json({
       success: false,
-      errors: { email: 'already exists',},
-    });
+      errors: { email: 'already exists' },
+    })
   }
 
-  const finalUser = new User(user);
+  const finalUser = new User(user)
 
-  return finalUser.save()
-    .then(() => res.send({ success: "true", user: finalUser.toAuthJSON() }))
-    .catch((e) => res.status(422).json({ success: false, errors: e, message: 'error saving user' }))
-});
-
+  return finalUser
+    .save()
+    .then(() => res.send({ success: 'true', user: finalUser.toAuthJSON() }))
+    .catch((e) =>
+      res
+        .status(422)
+        .json({ success: false, errors: e, message: 'error saving user' })
+    )
+})
 
 //assign students
 router.post('/assign', async (req, res, next) => {
+  const {
+    body: { user, students },
+  } = req
 
-  const { body: { user, students } } = req;
+  const query = await User.find({ _id: user._id })
 
-  const query = await User.find({_id: user._id});
-
-  User.updateOne({_id: query[0]._id}, {
-    students: [...students, ...query[0].students]
-  })
-  .then(() => res.send({ success: true }))
-  .catch((e) => res.status(422).json({ success: false, errors: e, message: 'error saving user' }))
-  
-});
+  User.updateOne(
+    { _id: query[0]._id },
+    {
+      students: [...students, ...query[0].students],
+    }
+  )
+    .then(() => res.send({ success: true }))
+    .catch((e) =>
+      res
+        .status(422)
+        .json({ success: false, errors: e, message: 'error saving user' })
+    )
+})
 
 //remove students
 router.post('/remove', async (req, res, next) => {
+  const {
+    body: { user, students },
+  } = req
 
-  const { body: { user, students } } = req;
+  const query = await User.find({ _id: user._id })
 
-  const query = await User.find({_id: user._id});
+  const remainingStudents = query[0].students.filter(
+    (student) => !students.includes(student.toString())
+  )
 
-  const remainingStudents = query[0].students.filter(student => !students.includes(student.toString()))
-
-  User.updateOne({_id: query[0]._id}, {
-    students: remainingStudents
-  })
-  .then(() => res.send({ success: true }))
-  .catch((e) => res.status(422).json({ success: false, errors: e, message: 'error saving user' }))
-  
-});
+  User.updateOne(
+    { _id: query[0]._id },
+    {
+      students: remainingStudents,
+    }
+  )
+    .then(() => res.send({ success: true }))
+    .catch((e) =>
+      res
+        .status(422)
+        .json({ success: false, errors: e, message: 'error saving user' })
+    )
+})
 
 //deactivate users
 router.post('/deactivate', async (req, res, next) => {
-  const { body:  { user } } = req;
-  const query = await User.find({_id: user._id});
+  const {
+    body: { user },
+  } = req
+  const query = await User.find({ _id: user._id })
   if (query.length == 0) {
     return res.status(422).json({
       success: false,
-      errors: { user: 'Does not exist',},
-    });
+      errors: { user: 'Does not exist' },
+    })
   }
-  User.updateOne({_id: query[0]._id}, {
-    isActive: false
-  })
-  .then(() => res.send({ success: true }))
-  .catch((e) => res.status(422).json({ success: false, errors: e, message: 'error saving user' }))
-});
+  User.updateOne(
+    { _id: query[0]._id },
+    {
+      isActive: false,
+    }
+  )
+    .then(() => res.send({ success: true }))
+    .catch((e) =>
+      res
+        .status(422)
+        .json({ success: false, errors: e, message: 'error saving user' })
+    )
+})
 
 // get all ambassadors, teachers
 router.post('/getAllFromRole', async (req, res, next) => {
-
-  const { body:  { role } } = req;
+  const {
+    body: { role },
+  } = req
 
   // console.log("res", role)
 
-  const list = await User.find({ role: role });
+  const list = await User.find({ role: role })
 
   // console.log("in getAllFromRole", list)
   const filteredList = list.map((user) => {
@@ -118,35 +149,42 @@ router.post('/getAllFromRole', async (req, res, next) => {
       email: user.email,
       firstName: user.firstName,
       lastName: user.lastName,
-      isActive: user.isActive
+      isActive: user.isActive,
+      _id: user._id,
     }
   })
 
   res.send({ success: true, list: filteredList })
-});
+})
 
 router.post('/update', async (req, res, next) => {
-
-  const { body: { user } } = req;
-  const query = await User.find({_id: user._id});
+  const {
+    body: { user },
+  } = req
+  const query = await User.find({ _id: user._id })
   if (query.length == 0) {
     return res.status(422).json({
       success: false,
-      errors: { user: 'Does not exist',},
-    });
+      errors: { user: 'Does not exist' },
+    })
   }
-  User.updateOne({_id: query[0]._id}, {
-    role: user.role,
-    students: user.students,
-    email: user.email,
-    firstName: user.firstName,
-    lastName: user.lastName,
-    isActive: user.isActive
-  })
-  .then(() => res.send({ success: true }))
-  .catch((e) => res.status(422).json({ success: false, errors: e, message: 'error saving user' }))
-
-
+  User.updateOne(
+    { _id: query[0]._id },
+    {
+      role: user.role,
+      students: user.students,
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      isActive: user.isActive,
+    }
+  )
+    .then(() => res.send({ success: true }))
+    .catch((e) =>
+      res
+        .status(422)
+        .json({ success: false, errors: e, message: 'error saving user' })
+    )
 })
 
 export default router
